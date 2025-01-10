a8925e85b1230f7ac5ab4f5ec2ccd00373f608a0
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-10 18:14:34 +0000

    initial set up including walking skeleton provided in class

---
+++ server/seed.js
@@ -0,0 +1,8 @@
+//We are going to send a SQL query to the database pool that we set up
+//We need to import the pool from the server file
+//We imported just ONE item from the server file. I signal this with {}
+import { db } from "./server.js";
+
+//Add your SQL queries here to create tables and add dummy data for testing purposes
+//! If you use the SQL editor on Supabase, please, copy and paste those queries here
+//If you have set up your database pool in the server, you need to import it here
